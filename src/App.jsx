5fe461acb4149d96d5fed0c588f1a94a28d88bdd
FrontendSuberu <joeysuberu@gmail.com>
2026-04-08 23:46:58 +0100

    added route paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,14 @@
-import logo from "/logo.svg";
 import "./App.css";
-
+import { Routes, Route } from "react-router-dom";
+import SignUp from "./pages/signup/SignUp";
+import Verify from "./pages/verify/Verify";
 
 function App() {
   return (
-    <main data-form>
-      <header className="verif-form-header">
-        <img src={logo} alt="ab33 logo" />
-      </header>
-
-      <div className="form-wrapper">
-        <h1>Sign up</h1>
-
-        <form method="post">
-          <div data-form-group>
-            <label htmlFor="fullName">
-              <span className="mono">FULL NAME</span>
-              <input type="text" />
-            </label>
-
-            <label htmlFor="email">
-              <span className="mono">EMAIL</span>
-              <input type="email" />
-            </label>
-
-            <label htmlFor="password">
-              <span className="mono">PASSWORD</span>
-              <input type="password" />
-            </label>
-          </div>
-
-          <span className="c-new-acct handwritten">
-            Don't have an account? create one here
-          </span>
-
-          <div className="form-btn-group">
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-              }}
-              className="mono"
-            >
-              Create my account
-            </button>
-
-            {/* <span className="handwritten">or</span>
-
-            <button className="mono">Continue with Google</button> */}
-          </div>
-        </form>
-      </div>
-    </main>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/verify" element={<Verify />} />
+      </Routes>
   );
 }
 

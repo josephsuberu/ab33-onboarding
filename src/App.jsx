63b15494c5528dfdf5ab5a850c9123ea101ffff9
FrontendSuberu <joeysuberu@gmail.com>
2026-04-08 01:39:18 +0100

    markup updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import logo from "/logo.svg";
 import "./App.css";
 
+
 function App() {
   return (
     <main data-form>
@@ -9,29 +10,43 @@ function App() {
       </header>
 
       <div className="form-wrapper">
-        <h1>Sign Up</h1>
+        <h1>Sign up</h1>
+
         <form method="post">
           <div data-form-group>
             <label htmlFor="fullName">
-              <span>FULL NAME</span>
+              <span className="mono">FULL NAME</span>
               <input type="text" />
             </label>
 
             <label htmlFor="email">
-              <span>EMAIL</span>
+              <span className="mono">EMAIL</span>
               <input type="email" />
             </label>
 
             <label htmlFor="password">
-              <span>PASSWORD</span>
+              <span className="mono">PASSWORD</span>
               <input type="password" />
             </label>
           </div>
 
-          <span className="c-new-acct">Don't have an account? create one here</span>
+          <span className="c-new-acct handwritten">
+            Don't have an account? create one here
+          </span>
 
           <div className="form-btn-group">
-            <button>Create my account</button>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+              }}
+              className="mono"
+            >
+              Create my account
+            </button>
+
+            {/* <span className="handwritten">or</span>
+
+            <button className="mono">Continue with Google</button> */}
           </div>
         </form>
       </div>

07ec8b1acbd82f6aff74ac7344e5cc69be72c1f1
FrontendSuberu <joeysuberu@gmail.com>
2026-04-09 16:26:43 +0100

    updated jsx with ui

--- src/pages/verify/Verify.jsx
+++ src/pages/verify/Verify.jsx
@@ -1,23 +1,35 @@
 import { Link } from "react-router-dom";
 import "./verify.css";
+import InputOtp from "./components/InputOtp";
 
 export default function Verify() {
   return (
     <main data-form id="verify">
       <form>
-        <span className="handwritten">We sent your verification code to</span>
+        <div>
+          <span className="handwritten">We sent your verification code to</span>
 
-        <div className="email-wrapper">joeysuberu@gmail.com</div>
+          <div className="email-wrapper">joeysuberu@gmail.com</div>
 
-        <div className="code-v">
-          <Link to="#" className="handwritten">
-            check yor email inbox here
-          </Link>
+          <div className="code-v">
+            <Link to="#" className="handwritten">
+              check yor email inbox here
+            </Link>
+          </div>
+
+          <InputOtp />
         </div>
 
-        <button type="submit" role="button" id="v-btn" className="mono">
-          Confirm my account
-        </button>
+        <div className="w-full">
+          <button
+            type="submit"
+            role="button"
+            id="v-btn"
+            className="mono text-[12px] w-full max-w-100"
+          >
+            Confirm my account
+          </button>
+        </div>
       </form>
     </main>
   );

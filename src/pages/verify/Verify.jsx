65cf18c543b5cfbb76eefae814acac7b8abff0cf
FrontendSuberu <joeysuberu@gmail.com>
2026-04-10 01:17:16 +0100

    updated codebase with custom hook

--- src/pages/verify/Verify.jsx
+++ src/pages/verify/Verify.jsx
@@ -1,34 +1,99 @@
 import { Link } from "react-router-dom";
 import "./verify.css";
 import InputOtp from "./components/InputOtp";
+import { useState, useRef } from "react";
+import { cn } from "@/lib/utils";
+import useAutoHeight from "./components/useAutoHeight";
+// import gsap from "gsap";
+// import { useGSAP } from "@gsap/react";
+// import { Flip } from "gsap/Flip";
 
 export default function Verify() {
+  const [otp, setOtp] = useState("");
+  const [otpBtn, setOtpBtn] = useState("Confirm my account");
+  const [notSubmitted, setNotSubmitted] = useState(true);
+  const { ref, height } = useAutoHeight();
+
+  const isComplete = otp.length === 5;
+
+  const updateOTP = (value) => {
+    setOtp(value);
+  };
+
+  // form submit
+  const handleOTP = (e) => {
+    e.preventDefault();
+
+    if (isComplete && otp === "12345") {
+      console.log("correct");
+      setOtp("");
+    } else {
+      console.log("wrong");
+      setOtp("");
+    }
+  };
+
+  // form button - interactive UI logic
+  const handleSubmitOTP = () => {
+    setOtpBtn("confirming");
+    document.activeElement?.blur(); // remove the focus from the OTP component if using keyboard navigation
+    setTimeout(() => {
+      setOtp("");
+      setOtpBtn(otpBtn);
+    }, 1300);
+
+    if (otp === "12345") {
+      setTimeout(() => {
+        setNotSubmitted(!notSubmitted);
+      }, 1300);
+    } else null;
+  };
+
   return (
     <main data-form id="verify">
-      <form>
-        <div>
-          <span className="handwritten">We sent your verification code to</span>
+      <form
+        onSubmit={handleOTP}
+        style={{
+          height: height ?? "auto",
+          overflow: "hidden",
+          transition: "height 1000ms cubic-bezier(0.65, 0, 0.35, 1)",
+        }}
+      >
+        <div className="inner-verify-form" ref={ref}>
+          <div>
+            <span className="handwritten">
+              We sent your verification code to
+            </span>
 
-          <div className="email-wrapper">joeysuberu@gmail.com</div>
+            <div className="email-wrapper">joeysuberu@gmail.com</div>
 
-          <div className="code-v">
-            <Link to="#" className="handwritten">
-              check yor email inbox here
-            </Link>
+            {notSubmitted && (
+              <div className="code-v">
+                <Link to="#" className="handwritten">
+                  check yor email inbox here
+                </Link>
+              </div>
+            )}
+
+            {notSubmitted && <InputOtp otp={otp} setOtp={updateOTP} />}
           </div>
 
-          <InputOtp />
-        </div>
-
-        <div className="w-full">
-          <button
-            type="submit"
-            role="button"
-            id="v-btn"
-            className="mono text-[12px] w-full max-w-100"
-          >
-            Confirm my account
-          </button>
+          {notSubmitted && (
+            <div className="w-full">
+              <button
+                type="submit"
+                role="button"
+                onClick={handleSubmitOTP}
+                disabled={!isComplete}
+                className={cn(
+                  "mono text-[12px] w-full max-w-100 v-btn",
+                  `${isComplete ? "bg-red-500" : "btn-disabled"}`,
+                )}
+              >
+                {otpBtn}
+              </button>
+            </div>
+          )}
         </div>
       </form>
     </main>

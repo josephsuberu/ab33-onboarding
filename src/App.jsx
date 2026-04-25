ee94f2d8c14a48d0c27a7ff63763d86b0789e764
FrontendSuberu <joeysuberu@gmail.com>
2026-04-25 10:56:31 +0100

    new route added to the app component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import SignUp from "./pages/signup/SignUp";
 import Verify from "./pages/verify/Verify";
+import PageView from "./components/ui/dashboard/PageView";
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={<SignUp />} />
         <Route path="/verify" element={<Verify />} />
+        <Route path="/sidebar" element={<PageView />} />
       </Routes>
   );
 }

306ca3c287ab379ae4d6c55adcd8f13bbc1a5beb
FrontendSuberu <joeysuberu@gmail.com>
2026-04-26 21:07:47 +0100

    codebase structure cleaned

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import SignUp from "./pages/signup/SignUp";
 import Verify from "./pages/verify/Verify";
-import PageView from "./components/ui/dashboard/PageView";
+import Layout from "./components/ui/dashboard/Layout";
+import DashboardHome from "./pages/dashboard/DashboardHome";
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<SignUp />} />
-        <Route path="/verify" element={<Verify />} />
-        <Route path="/sidebar" element={<PageView />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<SignUp />} />
+      <Route path="/verify" element={<Verify />} />
+
+
+
+      <Route path="/sidebar" element={<Layout />}>
+        <Route path="/sidebar" element={<DashboardHome />} />
+      </Route>
+    </Routes>
   );
 }
 
